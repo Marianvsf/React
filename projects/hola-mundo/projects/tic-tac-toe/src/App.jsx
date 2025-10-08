8eb570dc32265f33cf3eec14217258dcfc88e163
Marianvsf <marianvsf@gmail.com>
2025-10-07 20:00:52 -0400

    hola-mundo

---
+++ projects/hola-mundo/projects/tic-tac-toe/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square.jsx';
+import { TURNS } from './constants.js';
+import { checkEngGame, checkWinner } from './logic/board.js';
+import { WinnerModal } from './components/WinnerModal.jsx';
+import { saveGameToStorage, resetGameStorage } from './logic/storage/index.js';
+
+function App() {
+const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  });
+const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+  });
+const [winner, setWinner] = useState(null);
+
+const updateBoard = (index) => {
+
+  if (board[index] || winner) return
+  
+  const newBoard = [...board];
+  newBoard[index] = turn;
+  setBoard(newBoard);
+
+  const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+  setTurn(newTurn);
+  saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    });
+  
+  const newWinner = checkWinner(newBoard);
+  if (newWinner) {
+    confetti()
+    setWinner(newWinner)
+  } else if (checkEngGame(newBoard)) {
+    setWinner(false)
+  }
+};
+
+const resetGame = () => {
+  setBoard(Array(9).fill(null))
+  setTurn(TURNS.X)
+  setWinner(null)
+
+  resetGameStorage()
+}
+
+  return (
+    <main className='board'>
+      <h1>Tic Tac Toe</h1>
+      <button onClick={resetGame}>Reset del Juego</button>
+      <section className='game'>
+      {
+      board.map((square,index)=>{
+        return (
+          <Square
+          key={index}
+          index={index}
+          updateBoard={updateBoard}>
+            {board[index]}
+          </Square>
+        )
+      })
+      }
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+      <WinnerModal 
+      winner={winner}
+      resetGame={resetGame}/>
+    </main>
+  )
+}
+
+export default App
---
+++ projects/hola-mundo/projects/tic-tac-toe/src/components/Square.jsx
@@ -0,0 +1,13 @@
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : '' }`
+
+  const handleClik = () => {
+    updateBoard(index)
+  }
+  
+  return (
+    <div onClick={handleClik} className={className}>
+        {children}
+    </div>
+  )
+};
