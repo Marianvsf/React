1816a84f2bb4888c0c8018a6f44ba23c32a85049
Marianvsf <marianvsf@gmail.com>
2025-10-09 16:07:46 -0400

    button

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -2,49 +2,47 @@ import { useEffect, useState } from "react";
 import "./style.css";
 
 const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
-//const CAT_ENDOPOINYT_IMAG_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`;
-const CAT_PREFIX_IMAGE_URL = "https://cataas.com/cat/says";
 
 export function App() {
   const [fact, setFact] = useState();
   const [imageUrl, setImageUrl] = useState();
-  {
-    /* 
-  useEffect(() => {
-    async function getRandomFat() {
-      const res = await fetch(CAT_ENDOPOINYT_RANDOM_FACT);
-      const data = await res.json();
-      setFact(data.fact);
-    }
-    getRandomFat();
-  }, []);
-*/
-  }
-  useEffect(() => {
+  const [error, setError] = useState();
+
+  const getRandomFact = () => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          setError("No se ha podido recuperar la cita");
+        }
+        return res.json();
+      })
       .then((data) => {
         const { fact } = data;
         setFact(fact);
-
-        const threeFirstWord = fact.split(" ", 3).join(" ");
-        console.log(threeFirstWord);
-
-        fetch(
-          `https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red&json=true`
-        )
-          .then((res) => res.json())
-          .then((response) => {
-            console.log(response);
-            const { url } = response;
-            setImageUrl(url);
-          });
       });
-  }, []);
+  };
+  useEffect(getRandomFact, []);
+
+  useEffect(() => {
+    if (!fact) return;
+    const threeFirstWord = fact.split(" ", 3).join(" ");
+    console.log(threeFirstWord);
+
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWord}?fontSize=50&fontColor=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response);
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
     <main>
       <h1>App de Gatitos</h1>
+      <button onClick={getRandomFact}>Get new Fact</button>
       {fact && <p>{fact}</p>}
       {imageUrl && (
         <img
