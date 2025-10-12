a982e1a383f681aa07030ca429e40172e5138e56
Marianvsf <marianvsf@gmail.com>
2025-10-11 23:42:55 -0400

    render with mocks

--- projects/react-prueba-2/src/App.jsx
+++ projects/react-prueba-2/src/App.jsx
@@ -1,6 +1,10 @@
 import "./App.css";
+import responseMovies from "./mocks/with-results.json";
+import withoutResults from "./mocks/no-results.json";
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
     <div className="page">
       <header>
@@ -10,7 +14,21 @@ function App() {
           <button>Buscar</button>
         </form>
       </header>
-      <main>movies</main>
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <label>no hay movies</label>
+        )}
+      </main>
     </div>
   );
 }
