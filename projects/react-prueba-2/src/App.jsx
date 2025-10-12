ee2950ef136d1f0cc859abfcafd925ef84e62889
Marianvsf <marianvsf@gmail.com>
2025-10-12 14:12:10 -0400

    custom hooks

--- projects/react-prueba-2/src/App.jsx
+++ projects/react-prueba-2/src/App.jsx
@@ -1,16 +1,28 @@
 import "./App.css";
-import { useMovies } from "../../03-mouse-follower/src/hooks/useMovies.js";
 import { Movies } from "./components/Movies.jsx";
+import { useRef } from "react";
+import { useMovies } from "./hooks/useMovies.js";
 
 function App() {
   const { movies: mappedMovies } = useMovies();
+  const inputRef = useRef();
+
+  const handleSummit = (event) => {
+    event.preventDefault();
+    const inputEl = inputRef.current;
+    const value = inputEl.value;
+    console.log(value);
+  };
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className="form" action="submit">
-          <input placeholder="Avengers, Matrix, Harry Pott..."></input>
-          <button>Buscar</button>
+        <form className="form" onSubmit={handleSummit}>
+          <input
+            ref={inputRef}
+            placeholder="Avengers, Matrix, Harry Pott..."
+          ></input>
+          <button type="submit">Buscar</button>
         </form>
       </header>
       <main>
