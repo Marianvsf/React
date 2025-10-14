07b1955c3fa3b392c1c8e3e39648d664600abf87
Marianvsf <marianvsf@gmail.com>
2025-10-13 23:59:58 -0400

    save

--- projects/react-prueba-2/src/hooks/useMovies.js
+++ projects/react-prueba-2/src/hooks/useMovies.js
@@ -1,7 +1,7 @@
 import withoutResults from "../mocks/no-results.json";
 import { useState } from "react";
 
-export function useMovies({ search = "" } = {}) {
+export function useMovies({ search }) {
   const [responseMovies, setResponseMovies] = useState([]);
   const movies = responseMovies.Search;
   const mappedMovies = movies?.map((movie) => ({
