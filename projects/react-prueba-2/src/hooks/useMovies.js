8a2102a7c23a5d9958def26f398a4f23cbf8d565
Marianvsf <marianvsf@gmail.com>
2025-10-15 00:55:24 -0400

    useMemo

--- projects/react-prueba-2/src/hooks/useMovies.js
+++ projects/react-prueba-2/src/hooks/useMovies.js
@@ -7,20 +7,23 @@ export function useMovies({ search, sort }) {
   const [error, setError] = useState(null);
   const previousSearch = useRef(search);
 
-  const getMovies = async () => {
-    if (search === previousSearch.current) return;
-    try {
-      setLoading(true);
-      setError(null);
-      previousSearch.current = search;
-      const newMovies = await searchMovies({ search });
-      setMovies(newMovies);
-    } catch {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const getMovies = useMemo(() => {
+    return async () => {
+      if (search === previousSearch.current) return;
+      try {
+        setLoading(true);
+        setError(null);
+        previousSearch.current = search;
+        const newMovies = await searchMovies({ search });
+        setMovies(newMovies);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, [search]);
+
   /*const sortMovies = sort
     ? [...movies].sort((a, b) => a.title.localeCompare(b.title))
     : movies;*/
